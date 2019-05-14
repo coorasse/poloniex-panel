0016488ab63623fb4d457d5ca03fbda5ccef3d75
Alessandro Rodi <alessandro.rodi@renuo.ch>
2019-05-14 11:36:24 +0200

    Update apis

--- lib/currency_exchange_rate.rb
+++ lib/currency_exchange_rate.rb
@@ -5,8 +5,8 @@ class CurrencyExchangeRate
     if currency == 'USD'
       return 1
     else
-      resource = RestClient::Resource.new("http://free.currencyconverterapi.com/api/v5/convert?q=USD_#{currency}&compact=y")
-      JSON.parse(resource.get.body)["USD_#{currency}"]['val']
+      resource = RestClient::Resource.new("https://free.currconv.com/api/v7/convert?q=USD_#{currency}&compact=ultra&apiKey=#{ENV['CURRENCY_CONVERTER_API_KEY']}")
+      JSON.parse(resource.get.body)["USD_#{currency}"]
     end
   end
 end
